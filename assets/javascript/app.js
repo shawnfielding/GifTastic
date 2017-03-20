c948425b11b4c1186ea3631a061801efe844dedc
Shawn Fielding <shawnmfielding@gmail.com>
2017-03-20 10:03:01 -0400

    changed to https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 	function displayAnimals(ani) {
 		console.log("made it here")
 		theAni = ani.replace(/ /g, '+');
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + theAni + "&api_key=dc6zaTOxFJmzC+limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + theAni + "&api_key=dc6zaTOxFJmzC+limit=10";
 		$.ajax({
 			url: queryURL,
 			method: "GET"
